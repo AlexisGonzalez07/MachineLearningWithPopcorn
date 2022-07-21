1d8a08446e42fa2a1c3749aa822022a723981ad2
Victor Lupascu <victor.lupascu.1985@gmail.com>
2022-07-21 15:11:24 -0500

    create basic front end, setup flask server

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from app.routes import home
+
+def create_app(test_config=None):
+    #set up app config
+    app=Flask(__name__, static_url_path='/')
+    app.url_map.strict_slashes = False
+    app.config.from_mapping(
+        SECRET_KEY='super_secret_key'
+    )
+
+    #register routes
+    app.register_blueprint(home)  
+
+    return app
