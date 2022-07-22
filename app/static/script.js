b679216b5da3c66df60041c9b0dc51c91f629f1e
Victor Lupascu <victor.lupascu.1985@gmail.com>
2022-07-22 13:33:18 -0500

    add fetch requests to populate the homepage with newest releases

---
+++ app/static/script.js
@@ -0,0 +1,310 @@
+// movie news
+
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     'X-RapidAPI-Key': '7c400c61c9msh218a2ea93c6f1abp10adbajsn17a8bf4634c4',
+//     'X-RapidAPI-Host': 'movie-articles-and-news.p.rapidapi.com',
+//   },
+// }
+
+// fetch('https://movie-articles-and-news.p.rapidapi.com/articles', options)
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   .catch((err) => console.error(err))
+
+// =========================================================================================================================
+// movie titles
+
+response = {
+  titles: [
+    // {
+    //   id: 1593604,
+    //   title: 'The Sea Beast',
+    //   year: 2022,
+    //   imdb_id: 'tt9288046',
+    //   tmdb_id: 560057,
+    //   tmdb_type: 'movie',
+    //   type: 'movie',
+    // },
+    {
+      id: 1686822,
+      title: 'Girl in the Picture',
+      year: 2022,
+      imdb_id: 'tt21059520',
+      tmdb_id: 988046,
+      tmdb_type: 'movie',
+      type: 'movie',
+    },
+    // {
+    //   id: 1616014,
+    //   title: 'The Gray Man',
+    //   year: 2022,
+    //   imdb_id: 'tt1649418',
+    //   tmdb_id: 725201,
+    //   tmdb_type: 'movie',
+    //   type: 'movie',
+    // },
+    // {
+    //   id: 545187,
+    //   title: 'Bill Burr: Live at Red Rocks',
+    //   year: 2022,
+    //   imdb_id: 'tt21106500',
+    //   tmdb_id: 991115,
+    //   tmdb_type: 'movie',
+    //   type: 'movie',
+    // },
+    // {
+    //   id: 1679274,
+    //   title: 'Persuasion',
+    //   year: 2022,
+    //   imdb_id: 'tt13456318',
+    //   tmdb_id: 820912,
+    //   tmdb_type: 'movie',
+    //   type: 'movie',
+    // },
+    // {
+    //   id: 1686587,
+    //   title: 'Under the Amalfi Sun',
+    //   year: 2022,
+    //   imdb_id: 'tt15311262',
+    //   tmdb_id: 873127,
+    //   tmdb_type: 'movie',
+    //   type: 'movie',
+    // },
+    // {
+    //   id: 545316,
+    //   title: 'Nikki Glaser: Good Clean Filth',
+    //   year: 2022,
+    //   imdb_id: 'tt16160708',
+    //   tmdb_id: 993703,
+    //   tmdb_type: 'movie',
+    //   type: 'movie',
+    // },
+    // {
+    //   id: 545264,
+    //   title: "David A. Arnold: It Ain't for the Weak",
+    //   year: 2022,
+    //   imdb_id: 'tt21106704',
+    //   tmdb_id: 999237,
+    //   tmdb_type: 'movie',
+    //   type: 'movie',
+    // },
+    // {
+    //   id: 1687661,
+    //   title: 'Dickie V',
+    //   year: 2022,
+    //   imdb_id: 'tt21065336',
+    //   tmdb_id: 999368,
+    //   tmdb_type: 'movie',
+    //   type: 'movie',
+    // },
+    // {
+    //   id: 1625727,
+    //   title: "Anything's Possible",
+    //   year: 2022,
+    //   imdb_id: 'tt11464446',
+    //   tmdb_id: 787787,
+    //   tmdb_type: 'movie',
+    //   type: 'movie',
+    // },
+    // {
+    //   id: 3182399,
+    //   title: 'The Terminal List',
+    //   year: 2022,
+    //   imdb_id: 'tt11743610',
+    //   tmdb_id: 1002475,
+    //   tmdb_type: 'movie',
+    //   type: 'movie',
+    // },
+    // {
+    //   id: 1622320,
+    //   title: '1Up',
+    //   year: 2022,
+    //   imdb_id: 'tt13487922',
+    //   tmdb_id: 767401,
+    //   tmdb_type: 'movie',
+    //   type: 'movie',
+    // },
+    // {
+    //   id: 1686554,
+    //   title: "My Daughter's Killer",
+    //   year: 2022,
+    //   imdb_id: 'tt21049266',
+    //   tmdb_id: 992536,
+    //   tmdb_type: 'movie',
+    //   type: 'movie',
+    // },
+    // {
+    //   id: 1686819,
+    //   title: 'For Jojo',
+    //   year: 2022,
+    //   imdb_id: 'tt21044524',
+    //   tmdb_id: 968572,
+    //   tmdb_type: 'movie',
+    //   type: 'movie',
+    // },
+    // {
+    //   id: 1632034,
+    //   title: 'Z-O-M-B-I-E-S 3',
+    //   year: 2022,
+    //   imdb_id: 'tt14301252',
+    //   tmdb_id: 809107,
+    //   tmdb_type: 'movie',
+    //   type: 'movie',
+    // },
+    // {
+    //   id: 1685368,
+    //   title:
+    //     'The Day the Music Died: The Story of Don McLean\'s "American Pie"',
+    //   year: 2022,
+    //   imdb_id: 'tt12992162',
+    //   tmdb_id: 985709,
+    //   tmdb_type: 'movie',
+    //   type: 'movie',
+    // },
+    // {
+    //   id: 1255914,
+    //   title: 'Minions: The Rise of Gru',
+    //   year: 2022,
+    //   imdb_id: 'tt5113044',
+    //   tmdb_id: 438148,
+    //   tmdb_type: 'movie',
+    //   type: 'movie',
+    // },
+    // {
+    //   id: 1587954,
+    //   title: 'Valley of the Dead',
+    //   year: 2022,
+    //   imdb_id: 'tt10127708',
+    //   tmdb_id: 613093,
+    //   tmdb_type: 'movie',
+    //   type: 'movie',
+    // },
+    // {
+    //   id: 3165380,
+    //   title: 'Prehistoric Planet',
+    //   year: 2022,
+    //   imdb_id: 'tt10324164',
+    //   tmdb_id: 95171,
+    //   tmdb_type: 'tv',
+    //   type: 'movie',
+    // },
+    // {
+    //   id: 1620681,
+    //   title: 'The Black Phone',
+    //   year: 2022,
+    //   imdb_id: 'tt7144666',
+    //   tmdb_id: 756999,
+    //   tmdb_type: 'movie',
+    //   type: 'movie',
+    // },
+    {
+      id: 1601485,
+      title: 'Elvis',
+      year: 2022,
+      imdb_id: 'tt3704428',
+      tmdb_id: 614934,
+      tmdb_type: 'movie',
+      type: 'movie',
+    },
+    // {
+    //   id: 3187077,
+    //   title: 'Warriors on the Field',
+    //   year: 2022,
+    //   imdb_id: 'tt14673378',
+    //   tmdb_id: 997270,
+    //   tmdb_type: 'movie',
+    //   type: 'movie',
+    // },
+    {
+      id: 1584304,
+      title: 'Thor: Love and Thunder',
+      year: 2022,
+      imdb_id: 'tt10648342',
+      tmdb_id: 616037,
+      tmdb_type: 'movie',
+      type: 'movie',
+    },
+    // {
+    //   id: 1608765,
+    //   title: 'Lightyear',
+    //   year: 2022,
+    //   imdb_id: 'tt10298810',
+    //   tmdb_id: 718789,
+    //   tmdb_type: 'movie',
+    //   type: 'movie',
+    // },
+    // {
+    //   id: 1616827,
+    //   title: 'Sonic the Hedgehog 2',
+    //   year: 2022,
+    //   imdb_id: 'tt12412888',
+    //   tmdb_id: 675353,
+    //   tmdb_type: 'movie',
+    //   type: 'movie',
+    // },
+  ],
+  page: 1,
+  total_results: 4354,
+  total_pages: 175,
+}
+
+const imdb = (res) => {
+  res.forEach((el) => {
+    const url = `https://movie-details1.p.rapidapi.com/imdb_api/movie?id=${el.imdb_id}`
+    console.log(el.imdb_id)
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '7c400c61c9msh218a2ea93c6f1abp10adbajsn17a8bf4634c4',
+        'X-RapidAPI-Host': 'movie-details1.p.rapidapi.com',
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response.image)
+        // create a card for each item
+        // append to carousel container
+      })
+      .catch((err) => console.error(err))
+  })
+}
+
+imdb(response.titles)
+
+// ===========================================================================================================================
+
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     'X-RapidAPI-Key': '7c400c61c9msh218a2ea93c6f1abp10adbajsn17a8bf4634c4',
+//     'X-RapidAPI-Host': 'watchmode.p.rapidapi.com',
+//   },
+// }
+
+// fetch(
+//   'https://watchmode.p.rapidapi.com/list-titles/?types=movie&page=1&limit=7&sort_by=relevance_desc&release_date_start=20220101&release_date_end=20220722',
+//   options,
+// )
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   .catch((err) => console.error(err))
+
+//   ===========================================================================================================
+
+// ===========================================================================================================
+// imdb fetch
+
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '7c400c61c9msh218a2ea93c6f1abp10adbajsn17a8bf4634c4',
+// 		'X-RapidAPI-Host': 'movie-details1.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://movie-details1.p.rapidapi.com/imdb_api/movie?id=tt9288046', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
