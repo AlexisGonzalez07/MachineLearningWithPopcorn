2af8156613a4507e978ef8c54a42a2d79ed36962
Alexis Gonzalez <alexis.gonzalez07@gmail.com>
2022-07-25 10:26:02 -0500

    Split creation of genre column in dataframe to only create column first and then wrote another function to populate said column if value existed in the original genre column

--- 5000preprocess.py
+++ 5000preprocess.py
@@ -4,40 +4,50 @@ import json
 
 import pandas as pd
 df5000movies = pd.read_csv('../5000/tmdb_5000_movies.csv')
-
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
 
 def dropColumn(data, col):
-    data = data.dropna(subset=([col]))
+    data = data.drop(columns=[col])
     return data
-
-#homepage seems insignificant so I dropped it
-df5000movies = dropColumn(df5000movies, 'homepage')
-print(df5000movies.isnull().sum())
 
 #function to convert colum to json format
 def toJson(data, col, newCol):
     data[newCol] = data[data[col].notnull()][col].map(lambda x: json.loads(x))
-    return data[newCol]
-print(toJson(df5000movies, 'genres', 'json_genres'))
+    return data
+# print(toJson(df5000movies, 'genres', 'json_genres'))
+
+
+def makeGenreCol(data,col):
+    for j in range(len(data[col])):
+        jsonObj = data[col][j]
+        for i in range(len(jsonObj)):
+            name = str(jsonObj[i]['name']).lower()
+            if name not in data:
+                data[name] = pd.Series([0 for x in range(len(data[col]))])
+    return data
+                
+            
 
 
 #convert genre names in json object to columns
-def genre2Col(data, col):
-    #step through genre column
+def populateGenreCol(data, col):
     for j in range(len(data[col])):
-        #save json object
         jsonObj = data[col][j]
-        
-        #index into keyword name
+        # index into keyword name
         for i in range(len(jsonObj)):
-            #save name to a string obj
             name = str(jsonObj[i]['name']).lower()
-            #create new column if genre doesn't exist a column
-            if name not in data:
-                data[name] = 0
-                #add the column to the
-                data[name].loc[j]  = 1
-            else:
-                data[name].loc[j] = 1
-                
-genre2Col(df5000movies, "json_genres")
+            data.at[j, name] = 1
+    return data
+          
+#homepage seems insignificant so I dropped it
+
+def main(data):
+    data = dropColumn(data, 'homepage')     
+    data = toJson(data, 'genres', 'json_genres') 
+    data = dropColumn(data, 'genres')
+    data = makeGenreCol(data, "json_genres")
+    data = populateGenreCol(data, "json_genres") 
+    return data
+    
+    
+main(df5000movies)
