461ac323a278bb999e0137d7284bc17dd25a7f44
Victor Lupascu <victor.lupascu.1985@gmail.com>
2022-07-28 10:17:11 -0500

    add demo gif

--- app/static/homepage.js
+++ app/static/homepage.js
@@ -1,26 +1,33 @@
 // movie titles
 const carousel = document.querySelector('.carousel-inner')
 
-const imdb = async (el) => {
-  const url = `https://movie-details1.p.rapidapi.com/imdb_api/movie?id=${el.imdb_id}`
-  const m_info = await fetch(url, {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '7c400c61c9msh218a2ea93c6f1abp10adbajsn17a8bf4634c4',
-      'X-RapidAPI-Host': 'movie-details1.p.rapidapi.com',
-    },
-  })
+const imdb = async (el, i) => {
+  const posterArr = [
+    'https://m.media-amazon.com/images/M/MV5BOWY4MmFiY2QtMzE1YS00NTg1LWIwOTQtYTI4ZGUzNWIxNTVmXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_.jpg',
+    'https://i.ebayimg.com/images/g/uhsAAOSw-Cti1Mxu/s-l500.jpg',
+    'https://m.media-amazon.com/images/M/MV5BNzBjZGFkNTUtYTFlNS00Yjg4LWExNjEtODAxMjFmZTY0OGIyXkEyXkFqcGdeQXVyMjAwNzczNTU@._V1_FMjpg_UX1000_.jpg',
+    'https://images-na.ssl-images-amazon.com/images/I/81Xo-x5AT0L.__AC_SX300_SY300_QL70_FMwebp_.jpg',
+    'https://images-na.ssl-images-amazon.com/images/I/81zWZ6aB74L.__AC_SX300_SY300_QL70_FMwebp_.jpg',
+  ]
+  // const url = `https://movie-details1.p.rapidapi.com/imdb_api/movie?id=${el.imdb_id}`
+  // const m_info = await fetch(url, {
+  //   method: 'GET',
+  //   headers: {
+  //     'X-RapidAPI-Key': '7c400c61c9msh218a2ea93c6f1abp10adbajsn17a8bf4634c4',
+  //     'X-RapidAPI-Host': 'movie-details1.p.rapidapi.com',
+  //   },
+  // })
   // console.log(m_info)
-  if (m_info.ok) {
-    const poster = await m_info.json()
-    const innerDiv = document.createElement('div')
-    innerDiv.classList = 'carousel-item'
-    const carouselImg = document.createElement('img')
-    carouselImg.classList = 'd-block img-fluid mx-auto'
-    carouselImg.setAttribute('src', poster.image)
-    innerDiv.appendChild(carouselImg)
-    carousel.appendChild(innerDiv)
-  }
+  // if (m_info.ok) {
+  // const poster = await m_info.json()
+  const innerDiv = document.createElement('div')
+  innerDiv.classList = 'carousel-item'
+  const carouselImg = document.createElement('img')
+  carouselImg.classList = 'd-block img-fluid mx-auto'
+  carouselImg.setAttribute('src', posterArr[i])
+  innerDiv.appendChild(carouselImg)
+  carousel.appendChild(innerDiv)
+  // }
 }
 
 const fetchPosters = async () => {
@@ -57,15 +64,7 @@ const fetchPosters = async () => {
       tmdb_type: 'movie',
       type: 'movie',
     },
-    {
-      id: 545187,
-      title: 'Bill Burr: Live at Red Rocks',
-      year: 2022,
-      imdb_id: 'tt21106500',
-      tmdb_id: 991115,
-      tmdb_type: 'movie',
-      type: 'movie',
-    },
+
     {
       id: 1686554,
       title: "My Daughter's Killer",
@@ -75,15 +74,7 @@ const fetchPosters = async () => {
       tmdb_type: 'movie',
       type: 'movie',
     },
-    {
-      id: 1686587,
-      title: 'Under the Amalfi Sun',
-      year: 2022,
-      imdb_id: 'tt15311262',
-      tmdb_id: 873127,
-      tmdb_type: 'movie',
-      type: 'movie',
-    },
+
     {
       id: 1632034,
       title: 'Z-O-M-B-I-E-S 3',
@@ -93,43 +84,7 @@ const fetchPosters = async () => {
       tmdb_type: 'movie',
       type: 'movie',
     },
-    {
-      id: 1677314,
-      title: 'Prizefighter: The Life of Jem Belcher',
-      year: 2022,
-      imdb_id: 'tt4471908',
-      tmdb_id: 943822,
-      tmdb_type: 'movie',
-      type: 'movie',
-    },
-    {
-      id: 1686819,
-      title: 'For Jojo',
-      year: 2022,
-      imdb_id: 'tt21044524',
-      tmdb_id: 968572,
-      tmdb_type: 'movie',
-      type: 'movie',
-    },
-    {
-      id: 1587954,
-      title: 'Valley of the Dead',
-      year: 2022,
-      imdb_id: 'tt10127708',
-      tmdb_id: 613093,
-      tmdb_type: 'movie',
-      type: 'movie',
-    },
-    {
-      id: 1685368,
-      title:
-        'The Day the Music Died: The Story of Don McLean\'s "American Pie"',
-      year: 2022,
-      imdb_id: 'tt12992162',
-      tmdb_id: 985709,
-      tmdb_type: 'movie',
-      type: 'movie',
-    },
+
     {
       id: 1255914,
       title: 'Minions: The Rise of Gru',
@@ -139,45 +94,9 @@ const fetchPosters = async () => {
       tmdb_type: 'movie',
       type: 'movie',
     },
-    {
-      id: 1622320,
-      title: '1Up',
-      year: 2022,
-      imdb_id: 'tt13487922',
-      tmdb_id: 767401,
-      tmdb_type: 'movie',
-      type: 'movie',
-    },
-    {
-      id: 3165380,
-      title: 'Prehistoric Planet',
-      year: 2022,
-      imdb_id: 'tt10324164',
-      tmdb_id: 95171,
-      tmdb_type: 'tv',
-      type: 'movie',
-    },
-    {
-      id: 1601485,
-      title: 'Elvis',
-      year: 2022,
-      imdb_id: 'tt3704428',
-      tmdb_id: 614934,
-      tmdb_type: 'movie',
-      type: 'movie',
-    },
-    {
-      id: 1584304,
-      title: 'Thor: Love and Thunder',
-      year: 2022,
-      imdb_id: 'tt10648342',
-      tmdb_id: 616037,
-      tmdb_type: 'movie',
-      type: 'movie',
-    },
   ]
 
-  res.forEach((item) => imdb(item))
+  res.forEach((item, i) => imdb(item, i))
 }
 
 window.onload = fetchPosters
